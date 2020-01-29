368210a1d4cb0c70636b854fbc87c5ef1eaf0d0f
gkchahal <59899312+gkchahal@users.noreply.github.com>
2020-01-29 16:38:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var wrapperEle = document.body.querySelector(".wrapper");
+var checkEle = document.body.querySelector(".check");
+
+var totalbill = prompt("What is the total of you bill?");
+var baseCost = Number(totalbill);
+var tax = (baseCost * 0.07);
+var tip = (baseCost * 0.05);
+var checkTotal = baseCost + tax + tip;
+
+document.write("Total bill with tax and tip is: $" + checkTotal);
